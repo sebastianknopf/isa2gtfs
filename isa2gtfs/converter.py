29e25baa01809f0086dd09e4a799a044cdded7a9
sebastianknopf <mail@sebastian-knopf.de>
2024-09-25 22:56:23 +0200

    improved converter in order to handle ZIP files

--- isa2gtfs/converter.py
+++ isa2gtfs/converter.py
@@ -1,6 +1,8 @@
 import csv
 import logging
+import os
 import yaml
+import zipfile
 
 class IsaGtfsConverter:
 
@@ -27,15 +29,54 @@ class IsaGtfsConverter:
             self._config['mapping']['agency_id'] = 'agency-[agencyId]'
             self._config['mapping']['route_id'] = '[routeInternationalId]'
             self._config['mapping']['trip_id'] = '[routeId][tripId]'
+
+        self._txt_files = list()
         
-    def convert(self, input_directory, output_directory):
+    def convert(self, input, output):
+        
+        if input.endswith('.zip'):
+            input_directory = os.path.dirname(input)
+        else:
+            input_directory = input
+
+        if output.endswith('.zip'):
+            output_directory = os.path.dirname(output)
+        else:
+            output_directory = output
+        
+        if input.endswith('.zip'):
+            logging.info(f"unpacking ZIP archive {input} ...")
+
+            with zipfile.ZipFile(input, 'r') as zip_file:
+                zip_file.extractall(input_directory)
+
         if self._dialect == 'init51':
             from isa2gtfs.dialect import init51
             init51.convert(self, input_directory, output_directory)
         else:
             logging.error(f"unknown dialect {self._dialect}")
+
+        if output.endswith('.zip'):
+            logging.info(f"creating ZIP archive {output} ...")
+
+            with zipfile.ZipFile(output, 'w') as zip_file:
+                for txt_file in self._txt_files:
+                    zip_file.write(
+                        txt_file,
+                        os.path.basename(txt_file),
+                        compress_type=zipfile.ZIP_DEFLATED
+                    )
+
+                    os.remove(txt_file)
+
+        if input.lower().endswith('.zip'):
+            for file in os.listdir(input_directory):
+                if file.lower().endswith('.asc') or file.lower().endswith('.txt'):
+                    os.remove(os.path.join(input_directory, file))
     
     def _write_txt_file(self, txt_filename, txt_headers, txt_data):
+        self._txt_files.append(txt_filename)
+        
         with open(txt_filename, 'w', newline='', encoding='utf-8') as txt_file:
             csv_writer = csv.writer(txt_file, delimiter=',', quotechar='"')
             csv_writer.writerow(txt_headers)
